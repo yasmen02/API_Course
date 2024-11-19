5e0d84b171c64f4543cb8e7df5cd53af362d530d
yasmen zuheer <yasibrahem9@gmail.com>
2024-11-19 20:02:08 +0300

    vid 16

--- Api/routes/api.php
+++ Api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,11 +10,42 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//all routes / api here must be api authenticated
+Route::group(['middleware' => ['api'/*,'checkPassword',*/,'changeLanguage'], 'namespace' => 'Api'], function () {
+    Route::post('get-main-categories', 'CategoriesController@index');
+    Route::post('get-category-byId', 'CategoriesController@getCategoryById');
+    Route::post('change-category-status', 'CategoriesController@changeStatus');
+
+    Route::group(['prefix' => 'admin','namespace'=>'Admin'],function (){
+        Route::post('login', 'AuthController@login');
+
+        Route::post('logout','AuthController@logout') -> middleware(['auth.guard:admin-api']);
+          //invalidate token security side
+
+         //broken access controller user enumeration
+    });
+
+    Route::group(['prefix' => 'user','namespace'=>'User'],function (){
+        Route::post('login','AuthController@Login') ;
+    });
+
+
+    Route::group(['prefix' => 'user' ,'middleware' => 'auth.guard:user-api'],function (){
+       Route::post('profile',function(){
+           return  \Auth::user(); // return authenticated user data
+       }) ;
+
+
+    });
+
+});
+
+
+Route::group(['middleware' => ['api','checkPassword','changeLanguage','checkAdminToken:admin-api'], 'namespace' => 'Api'], function () {
+    Route::get('offers', 'CategoriesController@index');
 });
